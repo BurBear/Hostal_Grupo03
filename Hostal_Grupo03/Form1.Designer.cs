8739d031f170f9a5649b4df429b2c390a4d324d0
AlesSerrano <148881517+AlesSerrano@users.noreply.github.com>
2024-10-21 23:19:43 -0500

    Add files via upload

---
+++ Hostal_Grupo03/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace TipoHabitaciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            checkBox1 = new CheckBox();
+            richTextBox1 = new RichTextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(47, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(566, 169);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Datos Tipo de Habitación";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(20, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(105, 15);
+            label1.TabIndex = 0;
+            label1.Text = "ID Tipo Habitación";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(415, 18);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 35);
+            button1.TabIndex = 1;
+            button1.Text = "Agregar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(415, 65);
+            button2.Name = "button2";
+            button2.Size = new Size(107, 35);
+            button2.TabIndex = 2;
+            button2.Text = "Modificar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(415, 128);
+            button3.Name = "button3";
+            button3.Size = new Size(107, 35);
+            button3.TabIndex = 3;
+            button3.Text = "Cancelar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(131, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(24, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(138, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Nombre Tipo Habitación";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(24, 85);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(127, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(183, 65);
+            label3.Name = "label3";
+            label3.Size = new Size(139, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Costo Tipo de habitación";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(192, 85);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(130, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(462, 197);
+            button4.Name = "button4";
+            button4.Size = new Size(107, 34);
+            button4.TabIndex = 1;
+            button4.Text = "Nuevo";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(462, 237);
+            button5.Name = "button5";
+            button5.Size = new Size(107, 36);
+            button5.TabIndex = 2;
+            button5.Text = "Editar";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(462, 279);
+            button6.Name = "button6";
+            button6.Size = new Size(107, 34);
+            button6.TabIndex = 3;
+            button6.Text = "Desabilitar";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(23, 127);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(138, 19);
+            checkBox1.TabIndex = 6;
+            checkBox1.Text = "Estado de Habitación";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = SystemColors.ScrollBar;
+            richTextBox1.Location = new Point(47, 197);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(409, 140);
+            richTextBox1.TabIndex = 4;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkSeaGreen;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "MantenedorTipoHabitaciones";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private CheckBox checkBox1;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private RichTextBox richTextBox1;
+    }
+}
