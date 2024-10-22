a4e3d4175b779fc80990b3e8f8a60d947d2a1a85
BurBear <burbear03@gmail.com>
2024-10-22 00:59:04 -0500

    Se agrego Mantenedor Clientes

--- Hostal_Grupo03/MantenedordeHabitaciones .Designer.cs
+++ Hostal_Grupo03/MantenedordeHabitaciones .Designer.cs
@@ -46,9 +46,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Location = new System.Drawing.Point(8, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(182, 22);
+            this.label1.Size = new System.Drawing.Size(120, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tipo de Habitaciones";
             // 
@@ -56,11 +57,12 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(370, 31);
+            this.label2.Location = new System.Drawing.Point(247, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(219, 22);
+            this.label2.Size = new System.Drawing.Size(117, 16);
             this.label2.TabIndex = 0;
-            this.label2.Text = "Mantenedor Habitaciones";
+            this.label2.Text = "Hostal Las Mercedes\r\n";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // comboBox1
@@ -70,18 +72,20 @@
             "Matrimonial",
             "Suite normal",
             "Habitación normal"});
-            this.comboBox1.Location = new System.Drawing.Point(196, 115);
+            this.comboBox1.Location = new System.Drawing.Point(131, 75);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(186, 28);
+            this.comboBox1.Size = new System.Drawing.Size(125, 21);
             this.comboBox1.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 213);
+            this.label3.Location = new System.Drawing.Point(8, 138);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(168, 22);
+            this.label3.Size = new System.Drawing.Size(111, 16);
             this.label3.TabIndex = 0;
             this.label3.Text = "Costo Habitaciones";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -93,18 +97,20 @@
             "Matrimonial 100",
             "Suite normal 60 ",
             "Habitación normal 50"});
-            this.comboBox2.Location = new System.Drawing.Point(186, 240);
+            this.comboBox2.Location = new System.Drawing.Point(124, 156);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(186, 28);
+            this.comboBox2.Size = new System.Drawing.Size(125, 21);
             this.comboBox2.TabIndex = 1;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 334);
+            this.label4.Location = new System.Drawing.Point(8, 217);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(182, 22);
+            this.label4.Size = new System.Drawing.Size(119, 16);
             this.label4.TabIndex = 0;
             this.label4.Text = "Piso de la Habitación";
             this.label4.Click += new System.EventHandler(this.label3_Click);
@@ -117,62 +123,68 @@
             "2do Piso",
             "3ero Piso",
             "4to piso"});
-            this.comboBox3.Location = new System.Drawing.Point(186, 371);
+            this.comboBox3.Location = new System.Drawing.Point(124, 241);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(186, 28);
+            this.comboBox3.Size = new System.Drawing.Size(125, 21);
             this.comboBox3.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(692, 115);
+            this.button1.Location = new System.Drawing.Point(461, 75);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(218, 41);
+            this.button1.Size = new System.Drawing.Size(145, 27);
             this.button1.TabIndex = 2;
             this.button1.Text = "Agregar";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(692, 204);
+            this.button2.Location = new System.Drawing.Point(461, 133);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(218, 41);
+            this.button2.Size = new System.Drawing.Size(145, 27);
             this.button2.TabIndex = 2;
             this.button2.Text = "Editar";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(692, 300);
+            this.button3.Location = new System.Drawing.Point(461, 195);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(218, 41);
+            this.button3.Size = new System.Drawing.Size(145, 27);
             this.button3.TabIndex = 2;
             this.button3.Text = "Deshabilitar";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(502, 466);
+            this.button4.Location = new System.Drawing.Point(335, 303);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(218, 41);
+            this.button4.Size = new System.Drawing.Size(145, 27);
             this.button4.TabIndex = 2;
             this.button4.Text = "Aceptar y continuar";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(807, 466);
+            this.button5.Location = new System.Drawing.Point(538, 303);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(218, 41);
+            this.button5.Size = new System.Drawing.Size(145, 27);
             this.button5.TabIndex = 2;
             this.button5.Text = "Salir";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(1062, 528);
+            this.ClientSize = new System.Drawing.Size(708, 343);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button3);
@@ -185,6 +197,7 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form2";
             this.Text = "MantenedorHabitacion";
             this.Load += new System.EventHandler(this.Form2_Load);
